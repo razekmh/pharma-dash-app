2a679dcde5bcd7cfa1dbd542210c246eff01a8e8
mahmoud <razekmh@gmail.com>
2023-08-05 23:55:12 +0100

    display app title

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dash import Dash, html
 
+from src.components.layout import create_layout
 
 def main() -> None:
     app = Dash()
     app.title = "Pharama Dashboard"
-    app.layout = html.Div()
+    app.layout = create_layout(app)
     app.run()
 
 
